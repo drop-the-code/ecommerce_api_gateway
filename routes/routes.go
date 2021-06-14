4ddd5a92846671a1bf2314fb02427dafb0f06899
Vinicius Espindola <vinnyaoe@gmail.com>
2021-06-14 19:26:35 -0400

    fix permission access product

--- routes/routes.go
+++ routes/routes.go
@@ -9,12 +9,12 @@ func SetupRoutes(app *fiber.App) {
 
 	app.Post("/login", login)
 	app.Post("/user", userCreate)
-	app.Get("/product", productAll)
-	app.Post("/product", productCreate)
 
 	app.Use(jwtware.New(jwtware.Config{
 		SigningKey: []byte("secret"),
 	}))
+	app.Get("/product", productAll)
+	app.Post("/product", productCreate)
 
 	app.Delete("/user/:id", userDelete)
 	app.Get("/user", usersAll)
